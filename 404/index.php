feb2c84c81c8d73631f8a7e38c44b27456345f02
apps-klondikeprogrammers <master888stroke@gmail.com>
2022-08-22 20:19:25 +0100

    site

---
+++ 404/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Error 404</title>
+    <link href="/img/favicon1.ico" rel="shortcut icon" />
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="/css/main.min.css">
+	<style>
+        .disclaimer {display :none; }
+    </style>
+</head>
+<body>
+	<div class="backgroud_img">
+	<body style="background-image: url('/img/black.png');">
+	<a href="https://klondikeprogrammers.ga" class="return">Вернуться на главную страницу</a>	
+	<div class="numberError">
+		<h1 class="error" data-text="404">404</h1>
+	</div>
+	<h2 class="notFound">Страница не найдена</h2>
+    <br>
+    <h2 class="help" align="center">Ви наверно неправельно написали в адресной строке силку на страницу сайта</h2>
+</body>
+</html>
